1b96e30314c4b05c51e346c78969335a5b69cb38
greenastro <greenastro@naver.com>
2020-08-18 08:42:37 +0900

    Delete

--- 20200812/js/background.js
+++
@@ -1,8 +0,0 @@
-(function(){
-    const bodyElem = document.querySelector("body");
-    function getRandom(){
-        let number = Math.floor(Math.random()*5);
-        return number+1;
-    }
-    bodyElem.style.backgroundImage = "url('images/"+getRandom()+".jpg')";
-})();
